d693398879e000d70d3af36080cc79c926e13131
kyle <kyleflemington@gmail.com>
2017-11-30 21:24:58 -0800

    jquerying

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 	console.log("File Connected")
+
+
+
+//1. Press Submit and post to Body
+	$('#submit').click(function(e){
+		e.preventDefault()
+		console.log('clicked')
+		const val = $("#color_name").val()
+		$("post__Body").append
+	})
+
+
+//2.If input value is less than 3, not able to submit
+
+
+
+//3.If input value is less than 3, text color red
+
+
+//4.If input value is greater than 3, text color green
+
+
 })
